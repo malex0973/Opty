73e6d4bddfd34e6e29e6ab5bbd558850a04c800e
iafet <iafet@list.ru>
2023-02-18 08:47:01 +0300

    Add GLFW and create window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
+//
+// точка входа
+//
 
+#include <assert.h>
 
-int main(int, char**) {
+// #define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+int main(int argc, char **args)
+{
+    //if (!glfwInit())
+    //    return -1;
+
+    assert(glfwInit() && "Initialization GLFW failed");
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Opty", nullptr, nullptr);
+    assert(window && "Window or OpenGL context creation failed");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
